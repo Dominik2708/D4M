b92a05405040f8de681ea8b625b3cc206e2e1ea6
Dominik Schamber <dsch@marktplatz-agentur.de>
2025-09-26 11:26:52 +0200

    TASK: Fix css not loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-6xl font-poppins font-bold text-primary">Test</h1>
+      <h2 className="text-2xl font-avenir">Test</h2>
+    </div>
   );
 }
