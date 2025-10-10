2fbd3fab60fa6eab83c47a38f6b1c40b5af922ac
Dominik Schamber <dsch@marktplatz-agentur.de>
2025-10-10 10:23:31 +0200

    TASK: Install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Header } from '../components/Header';
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-6xl font-poppins font-bold text-primary">Test</h1>
-      <h2 className="text-2xl font-avenir">Test</h2>
+    <div className="min-h-screen bg-white">
+      <Header />
+      <main></main>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { useState } from 'react';
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 w-full bg-white/95 backdrop-blur-sm z-50 border-b border-gray-200"></header>
+  );
+}
