365e50aa325935ecdadc3ee662a4d9e427e02102
Dominik Schamber <dsch@marktplatz-agentur.de>
2025-11-07 08:57:08 +0100

    TASK: Add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { Footer } from '@/components/Footer';
 import { Header } from '../components/Header';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <main></main>
+      <main className="min-h-30"></main>
+      <Footer />
     </div>
   );
 }
